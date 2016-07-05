eeddca4f0a78fb3868d56426f4ef897a6f082274
hashmat MH <hashmat00@aol.com>
2016-07-05 22:15:33 +0000

    added my first Hello world

---
+++ helloReact/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static('public'));
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Server has started');
+});

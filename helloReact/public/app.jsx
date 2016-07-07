f7349fb48bdff1ff39b8f37903e829893f2e537f
hashmat MH <hashmat00@aol.com>
2016-07-07 21:29:34 +0000

    initial udpate on eact

--- helloReact/public/app.jsx
+++ helloReact/public/app.jsx
@@ -1,4 +1,61 @@
-      ReactDOM.render(
-          <h1>Hello React!</h1>,
-          document.getElementById('app')
-          );
+var Greeter = React.createClass({
+    getDefaultProps: function(){
+      return {
+          name: 'React',  
+          message: "This is teh default message "
+         };
+    
+        
+    },
+    getInitialState: function () {
+        
+      return{
+       name: this.props.name
+      }; 
+      
+    },
+    onButtonClick: function (e){
+        e.preventDefault();
+        
+        var nameRef = this.refs.name;
+        
+        var name = nameRef.value;
+        nameRef.value = '';
+        
+        if(typeof name === 'string' && name.length> 0){
+             this.setState({
+                
+                name: name
+            });
+        }
+        
+           
+        
+    },
+    render: function () {
+        var name = this.state.name;
+        var message = this.props.message;
+
+        return (
+            <div>
+            <h1>Hello {name}! </h1>
+            <p>{message + '!!'}</p>
+            
+            <form onSubmit={this.onButtonClick}>
+             <input type="text" ref="name"/>
+             <button>Submit</button>
+            </form>
+            
+            
+            </div>
+        );
+    }
+});
+
+var firstName = 'Johnny';
+
+
+ReactDOM.render(
+ <Greeter name={firstName}/>,
+ document.getElementById('app')
+);

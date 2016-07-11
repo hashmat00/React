d5dc10151de332b099b0710a0820a6fbb1ab9301
hashmat MH <hashmat00@aol.com>
2016-07-11 22:22:18 +0000

    added babel-stage-0 to webpack

--- ReactBoilerplate/app/app.jsx
+++ ReactBoilerplate/app/app.jsx
@@ -1,6 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+
+var objOne = {
+  name: 'hashmat',
+  location: 'antioch'
+};
+
+
+var objTwo = {
+  age: 25,
+  ...objOne
+};
+
+
+console.log(objTwo);
+
+
 ReactDOM.render(
   <h1>Boilerplate app!</h1>,
   document.getElementById('app')

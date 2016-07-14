393b00aadbf171b4a6ecb8e5b6bbbb75c4d87015
hashmat MH <hashmat00@aol.com>
2016-07-14 20:12:00 +0000

    added modal error popup

--- ReactBoilerplate/app/components/WeatherMessage.jsx
+++ ReactBoilerplate/app/components/WeatherMessage.jsx
@@ -1,28 +1,9 @@
 var React = require('react');
 
-// var WeatherMessage = React.createClass({
-    
-//     render: function(){
-        
-//         var {location, temp} = this.props;
-        
-//         return (
-            
-//             <h3>Its {temp} in {location} </h3>
-//             )
-//     }
-// });
-
-
-var WeatherMessage = ({location, temp}) => {
-    // var {location, temp} = props;  // removed the "this" from this.props
-        
-        return (
-            
-            <h3>Its {temp} in {location} </h3>
-            )
-}
-
-
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <h3 className="text-center">It's it {temp} in {location}.</h3>
+  )
+};
 
 module.exports = WeatherMessage;
